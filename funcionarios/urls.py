5948ba06b161b3f9d53ee02e1138242f1763de77
Enderson Jackson <endersonjackson2@gmail.com>
2023-09-25 16:22:57 -0300

    Add Salvar Funcionariov2

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -5,9 +5,13 @@ from funcionarios.views import *
 urlpatterns = [
     path('procurar/', procurar_pessoal, name='procurar_pessoal'),
     path('novo/funcionario/', cadastrar_funcionarios_pessoal, name='cadastrar_funcionarios_pessoal'),
+    path('novo/funcionario/add', add_funcionario_v2, name='add_funcionario_v2'),
     path('novo/cargo/', cadastrar_cargo_pessoal, name='cadastrar_cargo_pessoal'),
     path('impressoes/', impressoes_pessoal, name='impressoes_pessoal'),
     path('relatorios/', relatorios_pessoal, name='relatorios_pessoal'),
+    
+    
+    
     path('htmx/add_inputs_dependente/<int:id>/', add_inputs_dependente, name='add_inputs_dependente'),
     path('htmx/clear_element/', clear_element, name='clear_element'),
     
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from django.http import HttpResponse
+from django.urls import reverse
 from django.views.generic.base import TemplateView
 from funcionarios.forms import *
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,7 +8,7 @@ from django.http.response import JsonResponse
 from django.urls.base import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from funcionarios.models import *
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 
 def procurar_pessoal(request, template_name="funcionarios/fragmentos/procurar/procurar_home.html"):
@@ -48,7 +50,370 @@ def cadastrar_funcionarios_pessoal(request, template_name= 'funcionarios/fragmen
         }
         return render(request, template_name, context)
 
+def add_funcionario_v2(request):
+    
+    if request.htmx:
+        menu_ativo = 'CADASTRARFUNCIONARIOS'
+        
+        #validar Nome vazio
+        if request.POST.get('nome') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Nome' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        
+        #validar Nome Pequeno
+        elif len(request.POST.get('nome')) <= 3:
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Nome' muito curto"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            nome = request.POST.get('nome')
+            
+        
+         #validar CPF
+        if request.POST.get('cpf') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'CPF' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        
+        # validar CPF incompleto
+        elif len(request.POST.get('cpf')) != 14:
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'CPF' inválido"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            cpf = request.POST.get('cpf')
+            
+            
+        #validar Data de Nascimento
+        if request.POST.get('data_nascimento') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Data de Nascimento' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            data_nascimento = datetime.strptime(request.POST.get('data_nascimento'), '%Y-%m-%d')
+            
+        #validar Endereço
+        if request.POST.get('endereco') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Endereço' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            endereco = request.POST.get('endereco')
+               
+         
+        #validar Matricula
+        if request.POST.get('matricula') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Matrícula' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            matricula = request.POST.get('matricula')  
+            
+            
+        #validar Tipo de Contrato
+        if request.POST.get('tipo_contrato') in ['','-1', None] :
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Selecione Tipo de Contrato"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            tipo_contrato = request.POST.get('tipo_contrato')   
+            
+            if tipo_contrato == 'DETERMINADO':
+                 #validar data_inicio_prorrogacao
+                if request.POST.get('data_inicio_prorrogacao') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Campo 'Início Prorrogação' Vazio"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    data_inicio_prorrogacao = datetime.strptime(request.POST.get('data_inicio_prorrogacao'), '%Y-%m-%d')
+                 #validar data_fim_prorrogacao
+                if request.POST.get('data_fim_prorrogacao') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Campo 'Fim Prorrogação' Vazio"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    data_fim_prorrogacao = datetime.strptime(request.POST.get('data_fim_prorrogacao'), '%Y-%m-%d')
+            else:
+                data_inicio_prorrogacao = ''
+                data_fim_prorrogacao = ''
 
+        #validar Data de Admissão
+        if request.POST.get('data_admissao') == "":
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Data de Admissão' Vazio"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            data_admissao = datetime.strptime(request.POST.get('data_admissao'), '%Y-%m-%d')
+            
+            
+        #validar Cargo
+        if request.POST.get('cargo') in ['','-1', None] :
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Selecione Cargo"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            cargo = Cargo.objects.get(pk = int(request.POST.get('cargo'))) 
+            
+        #validar Salário
+        if request.POST.get('salario') in ['','0', None] :
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Campo 'Salário' Vazio ou Inválido"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            salario = float(request.POST.get('salario').replace('.','').replace(',','.'))
+            
+            
+            
+        #validar Lotação
+        if request.POST.get('lotacao') in ['','-1', None] :
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Selecione Lotação/Centro de Custo"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            lotacao = Obra.objects.get(pk = int(request.POST.get('lotacao')))
+            
+        #validar Situacao
+        if request.POST.get('situacao') in ['','-1', None] :
+            context = {
+            'menu_ativo' : menu_ativo,
+            'text_error': "Selecione Situação"
+             }
+            
+            response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+            response['HX-Retarget'] = '#error-container'
+            return response
+        else:
+            situacao = request.POST.get('situacao')
+            if situacao == 'DEMITIDO' :
+                 #validar data_demissao
+                if request.POST.get('data_demissao') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Campo 'Data de Rescisão' Vazio"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    data_demissao = datetime.strptime(request.POST.get('data_demissao'), '%Y-%m-%d')
+                 #validar tipo_demissao
+                if request.POST.get('tipo_demissao') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Selecione Tipo de Rescisão"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    tipo_demissao = request.POST.get('tipo_demissao')
+                    data_inicio_afastamento = ''
+                    data_fim_afastamento = ''
+                   
+            elif situacao == 'AFASTADO INSS - POR DOENÇA' or situacao == 'AFASTADO INSS - POR ACIDENTE':
+                 #validar data_inicio_afastamento
+                if request.POST.get('data_inicio_afastamento') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Campo 'Início Afastamento' Vazio"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    data_inicio_afastamento = datetime.strptime(request.POST.get('data_inicio_afastamento'), '%Y-%m-%d')
+                 #validar data_fim_afastamento
+                if request.POST.get('data_fim_afastamento') in ['','-1', None] :
+                    context = {
+                    'menu_ativo' : menu_ativo,
+                    'text_error': "Campo 'Fim Afastamento' Vazio"
+                    }
+                    
+                    response =  render(request, template_name='funcionarios/fragmentos/funcionarios/error_form_funcionario.html', context=context)
+                    response['HX-Retarget'] = '#error-container'
+                    return response
+                else:
+                    data_fim_afastamento = datetime.strptime(request.POST.get('data_fim_afastamento'), '%Y-%m-%d')
+                    data_demissao = ''
+                    tipo_demissao = ''
+            else:
+                data_demissao = ''
+                tipo_demissao = ''
+                data_inicio_afastamento = ''
+                data_fim_afastamento = ''
+        
+        #validar Responsavel direto
+        if request.POST.get('responsavel_direto') in ['','-1', None]:
+            responsavel_direto = None
+        else:
+            responsavel_direto = ResponsávelObraFuncionariov2.objects.get(pk = int(request.POST.get('responsavel_direto')))  
+            
+        #validar Banco
+        if request.POST.get('banco') in ['','-1', None]:
+            banco = None
+        else:
+            responsavel_direto = Banco.objects.get(pk = int(request.POST.get('banco')))
+            
+            
+        #validar tipo_responsavel
+        if request.POST.get('tipo_responsavel') == 'on':
+            tipo_responsavel = True 
+        else:
+            responsavel_direto = False
+            
+        #validar analfabeto
+        if request.POST.get('analfabeto') == 'on':
+            analfabeto = True 
+        else:
+            analfabeto = False
+            
+            
+
+                
+               
+        rg = request.POST.get('rg', '')
+        ctps = request.POST.get('ctps', '')
+        cnh = request.POST.get('cnh', '')
+        categoria_cnh = request.POST.get('categoria_cnh', '')
+        cep_endereco = request.POST.get('cep_endereco', '')
+        telefone1 = request.POST.get('telefone1', '')
+        telefone2 = request.POST.get('telefone2', '')
+        
+        adicional = float(request.POST.get('adicional', '0').replace('.','').replace(',','.'))
+       
+              
+        
+        agencia = request.POST.get('agencia')
+        tipo_conta = request.POST.get('tipo_conta')
+        conta = request.POST.get('conta')
+        operacao = request.POST.get('operacao')
+        pix = request.POST.get('pix')
+        tipo_pix = request.POST.get('tipo_pix')
+        
+        esocial = request.POST.get('esocial')
+   
+        obs = request.POST.get('obs')
+        
+        nomes_dependentes = request.POST.getlist('nome_dependente')
+        cpfs_dependentes = request.POST.getlist('cpf_dependente')
+ 
+            
+        #CRIAR NOVO FUNCIONÁRIO
+        FuncionarioV2.objects.create(
+            nome = nome,
+            cpf = cpf,
+            rg = rg,
+            ctps = ctps,
+            cnh = cnh,
+            categoria_cnh = categoria_cnh,
+            # data_nascimento = data_nascimento,
+            endereco = f'{endereco} (CEP:{cep_endereco})',
+            telefone1 = telefone1,
+            telefone2 = telefone2,
+            matricula = matricula,
+            tipo_contrato = tipo_contrato,
+            # data_admissao = data_admissao,
+            # data_inicio_prorrogacao = data_inicio_prorrogacao,
+            # data_fim_prorrogacao = data_fim_prorrogacao,
+            cargo = cargo,
+            salario = salario,
+            adicional = float(adicional),
+            lotacao = lotacao,
+            tipo_responsavel = tipo_responsavel,
+            situacao = situacao,
+            # data_inicio_afastamento = data_inicio_afastamento ,
+            # data_fim_afastamento = data_fim_afastamento,
+            # data_demissao = data_demissao,
+            tipo_demissao = tipo_demissao,            
+            banco = banco, 
+            agencia = agencia,
+            tipo_conta = tipo_conta,
+            conta = conta,
+            op = operacao,
+            pix = pix,
+            tipo_pix = tipo_pix,
+            esocial = esocial,
+            analfabeto = analfabeto,
+            responsavel_direto = responsavel_direto,
+            obs = obs,          
+        )    
+        
+        return redirect(reverse('procurar_pessoal'))  
+           
 
 def cadastrar_cargo_pessoal(request, template_name= 'funcionarios/fragmentos/cargos/cargos_home.html'):
     if request.method == 'GET':
